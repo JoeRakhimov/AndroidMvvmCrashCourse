7c887a33f103b0ce7a6a32742fe73fc0d4748959
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2019-12-24 17:12:19 +0500

    Added architecture component viewmodel for example task

--- app/src/main/java/com/joerakhimov/mvvmcrashcourse/t1_simple_task/CouponActivity.kt
+++ app/src/main/java/com/joerakhimov/mvvmcrashcourse/t1_simple_task/CouponActivity.kt
@@ -6,16 +6,19 @@ import android.text.TextWatcher
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.joerakhimov.mvvmcrashcourse.R
 import kotlinx.android.synthetic.main.activity_coupon.*
 
 class CouponActivity : AppCompatActivity() {
 
-    val viewModel: CouponViewModel? = null
+    var viewModel: CouponViewModel? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coupon)
+
+        viewModel = ViewModelProviders.of(this)[CouponViewModel::class.java]
 
         viewModel?.init()
 
